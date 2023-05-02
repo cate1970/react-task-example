1abdc25209b2c9a93281b3ce8e53e9924b5f18cf
Carlos Terán <carlos.teran.gutierrez@gmail.com>
2023-05-01 20:24:27 -0500

    First commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/task";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+    console.log(tasks);
+    console.log(taskId);
+  }
+
+  let x = 70;
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        createTask,
+        deleteTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
+
+export default { TaskContext };
---
+++ src/data/task.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+      id: 0,
+      title: "Primera tarea",
+      description: "Mi primera tarea"  
+    },
+    {
+        id: 1,
+        title: "Segunda tarea",
+        description: "Mi primera tarea"  
+    },
+    {
+        id: 2,
+        title: "Tercera tarea",
+        description: "Mi primera tarea"  
+      },
+      {
+        id: 3,
+        title: "Cuarta tarea",
+        description: "Mi primera tarea"  
+      },
+]
